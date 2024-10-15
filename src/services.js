0adff03f5b7e3968f4206da7b434c5153b3ed3c7
siltukoley <siltukoley621@gmail.com>
2024-10-16 02:48:29 +0530

    modify service.js

--- src/services.js
+++ src/services.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const server = "http://localhost:3001/api/download";
+const server = "https://idown-backend.onrender.com/api/download";
 
 const getVideoDownloadUrl = async url => {
   const response = await axios.post(server, { url: url });
